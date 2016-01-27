1bc60203122b106da96d1fedfbe01a74fbe130a9
Michael Hauptmann <mhauptma73@gmail.com>
2016-01-27 18:16:43 +0100

    Added MemberBundle

---
+++ src/MemberBundle/Entity/Member.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace MemberBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Member
+ */
+class Member
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=100)
+     */
+    protected $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="surname", type="string", length=100)
+     */
+    protected $surname;
+
+    /**
+    * @var User
+    *
+    * @ORM\OneToOne(targetEntity="\UserBundle\Entity\User")
+    */
+    protected $User;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Member
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set surname
+     *
+     * @param string $surname
+     *
+     * @return Member
+     */
+    public function setSurname($surname)
+    {
+        $this->surname = $surname;
+
+        return $this;
+    }
+
+    /**
+     * Get surname
+     *
+     * @return string
+     */
+    public function getSurname()
+    {
+        return $this->surname;
+    }
+
+    /**
+     * Set user
+     *
+     * @param \UserBundle\Entity\User $user
+     *
+     * @return Member
+     */
+    public function setUser(\UserBundle\Entity\User $user = null)
+    {
+        $this->User = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return \UserBundle\Entity\User
+     */
+    public function getUser()
+    {
+        return $this->User;
+    }
+}

cf69cc47fe63f37641f3de06d5917c46b1c8b852
Michael Hauptmann <mhauptma73@gmail.com>
2016-02-11 14:41:17 +0100

    Added Js Translation Bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
 
+            new Bazinga\Bundle\JsTranslationBundle\BazingaJsTranslationBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             
             new Sonata\CoreBundle\SonataCoreBundle(),

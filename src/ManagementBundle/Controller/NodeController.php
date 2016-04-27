7cee8e8c5b90b0c373dca9fbb3652891fbdbb7a2
Michael Hauptmann <mhauptma73@gmail.com>
2016-04-27 09:57:48 +0200

    Added Node management

---
+++ src/ManagementBundle/Controller/NodeController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace ManagementBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+
+use NodeBundle\Entity\Node;
+use ManagementBundle\Form\NodeType;
+
+/**
+ * @Route("/node")
+ */
+class NodeController extends Controller
+{
+    /**
+     * @Route("/edit")
+     * @Security("has_role('ROLE_MANAGEMENT')")
+     */
+    public function editAction(Request $request)
+    {
+        $breadcrumbs = $this->get("white_october_breadcrumbs");
+        $breadcrumbs->addItem("Home", $this->get("router")->generate("homepage"));
+        $breadcrumbs->addItem("Management", $this->get("router")->generate("management_default_index"));
+        $breadcrumbs->addItem("Node", $this->get("router")->generate("management_node_edit"));
+
+        $em = $this->getDoctrine()->getManager();
+
+        $node_id = $this->getUser()->getNode()->getId();
+        $node = $em->getRepository('NodeBundle:Node')
+            ->createQueryBuilder('n')
+            ->select('n,l')
+            ->leftJoin('n.Location', 'l')
+            ->where('n.id = :node')
+            ->setParameter('node', $node_id)
+            ->getQuery()
+            ->getResult()
+        ;
+        $node = $node[0];
+
+        $form = $this->createForm(NodeType::class, $node);
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em->persist($node);
+            $em->flush();
+
+            if ($form->get('saveAndClose')->isClicked()) {
+                $url = $this->generateUrl('management_default_index');
+            }else{
+                $url = $this->generateUrl('management_node_edit', array('id'=>$node->getId()));
+            }
+            $response = new RedirectResponse($url);
+
+            return $response;
+        }
+
+        return $this->render('ManagementBundle:Node:edit.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+}
---
+++ src/ManagementBundle/Form/NodeType.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace ManagementBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ResetType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Doctrine\ORM\EntityRepository;
+
+use LocationBundle\Form\LocationType;
+
+class NodeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('address')
+            ->add('Location', LocationType::class)
+            ->add('save', SubmitType::class, array(
+                'translation_domain' => 'messages',
+                'attr' => array('btn'=>'buttons')
+            ))
+            ->add('saveAndClose', SubmitType::class, array(
+                'translation_domain' => 'messages',
+                'attr' => array('btn'=>'buttons')
+            ))
+            ->add('cancel', ResetType::class, array(
+                'translation_domain' => 'messages',
+                'attr' => array(
+                    'btn' => 'buttons',
+                    'class' => 'btn-danger cancel-btn',
+                    'data-path' => 'manager_visit_index'
+                ),
+                'label' => 'Close'
+            ))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+	{
+	    $resolver->setDefaults(array(
+	        'data_class' => 'NodeBundle\Entity\Node',
+            'translation_domain' => 'node'
+	    ));
+	}
+}

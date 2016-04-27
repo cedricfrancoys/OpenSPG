3ade97ab94d1c54224470b333bb3011df97eb139
Michael Hauptmann <mhauptma73@gmail.com>
2016-04-27 10:37:23 +0200

    Adde HelpTypeExtension

---
+++ src/AppBundle/Form/Extension/HelpTypeExtension.php
@@ -0,0 +1,46 @@
+<?php
+namespace AppBundle\Form\Extension;
+
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\PropertyAccess\PropertyAccess;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+
+class HelpTypeExtension extends AbstractTypeExtension
+{
+    /**
+     * Returns the name of the type being extended.
+     *
+     * @return string The name of the type being extended
+     */
+    public function getExtendedType()
+    {
+        return FormType::class;
+    }
+
+    /**
+     * Add the help option
+     *
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefined(array('help'));
+    }
+
+    /**
+     * Pass the help text to the view
+     *
+     * @param FormView $view
+     * @param FormInterface $form
+     * @param array $options
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        if (array_key_exists('help', $options)) {
+            $view->vars['help'] = $options['help'];
+        }
+    }
+}

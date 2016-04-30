4394fd5ae5764d54145c399f2e661a5ab41138cd
Michael Hauptmann <mhauptma73@gmail.com>
2016-04-30 03:33:02 +0200

    Fixes to Producer/Visits

--- src/ProducerBundle/Controller/VisitController.php
+++ src/ProducerBundle/Controller/VisitController.php
@@ -19,7 +19,7 @@ use ProducerBundle\Form\VisitType;
 class VisitController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", options={"expose":true})
      * @Security("has_role('ROLE_PRODUCER')")
      */
     public function indexAction()
@@ -40,91 +40,109 @@ class VisitController extends Controller
             ->leftJoin('v.Property', 'pr')
             ->leftJoin('pr.Member', 'm2')
             ->leftJoin('m2.User', 'u2')
-            ->where('p.User = :user OR m2.User = :user')
+            ->where('u = :user OR u2 = :user')
             ->setParameter('user', $this->getUser())
             ->getQuery()
             ->getResult();
 
         return $this->render('ProducerBundle:Visit:index.html.twig', array(
-            'visits' => $visits
+            'visits' => $visits,
+            'menu' => 'account'
         ));
     }
 
     /**
      * @Route("/add")
      */
-    public function addAction(Request $request)
-    {
-        $breadcrumbs = $this->get("white_october_breadcrumbs");
-        $breadcrumbs->addItem("Home", $this->get("router")->generate("homepage"));
-        $breadcrumbs->addItem("Producer", $this->get("router")->generate("producer_member_index"));
-        $breadcrumbs->addItem("Visits", $this->get("router")->generate("producer_visit_index"));
-        $breadcrumbs->addItem("Add", $this->get("router")->generate("producer_visit_add"));
+    // public function addAction(Request $request)
+    // {
+    //     $breadcrumbs = $this->get("white_october_breadcrumbs");
+    //     $breadcrumbs->addItem("Home", $this->get("router")->generate("homepage"));
+    //     $breadcrumbs->addItem("Producer", $this->get("router")->generate("producer_member_index"));
+    //     $breadcrumbs->addItem("Visits", $this->get("router")->generate("producer_visit_index"));
+    //     $breadcrumbs->addItem("Add", $this->get("router")->generate("producer_visit_add"));
 
-        $visit = new Visit();
-        $visit->setVisitDate(new \DateTime());
-        $visit->setStartTime(new \DateTime());
-        $endTime = new \DateTime();
-        $endTime->add(new \DateInterval('PT4H'));
-        $visit->setEndTime($endTime);
+    //     $visit = new Visit();
+    //     $visit->setVisitDate(new \DateTime());
+    //     $visit->setStartTime(new \DateTime());
+    //     $endTime = new \DateTime();
+    //     $endTime->add(new \DateInterval('PT4H'));
+    //     $visit->setEndTime($endTime);
 
-        $form = $this->createForm(VisitType::class, $visit);
-        // $form->setData($user);
+    //     $form = $this->createForm(VisitType::class, $visit);
+    //     // $form->setData($user);
 
-        $form->handleRequest($request);
+    //     $form->handleRequest($request);
 
-        if ($form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
+    //     if ($form->isValid()) {
+    //         $em = $this->getDoctrine()->getManager();
 
-            $em->persist($visit);
-            $em->flush();
+    //         $em->persist($visit);
+    //         $em->flush();
 
-            if (null === $response = $event->getResponse()) {
-                $url = $this->generateUrl('producer_visit_edit');
-                $response = new RedirectResponse($url);
-            }
+    //         if (null === $response = $event->getResponse()) {
+    //             $url = $this->generateUrl('producer_visit_edit');
+    //             $response = new RedirectResponse($url);
+    //         }
 
-            return $response;
-        }
+    //         return $response;
+    //     }
 
-        return $this->render('ProducerBundle:Visit:edit.html.twig', array(
-            'form' => $form->createView()
-        ));
-    }
+    //     return $this->render('ProducerBundle:Visit:edit.html.twig', array(
+    //         'form' => $form->createView()
+    //     ));
+    // }
 
     /**
      * @Route("/{id}")
      */
-    public function editAction(Request $request, $id)
+    // public function editAction(Request $request, $id)
+    // {
+    //     $breadcrumbs = $this->get("white_october_breadcrumbs");
+    //     $breadcrumbs->addItem("Home", $this->get("router")->generate("homepage"));
+    //     $breadcrumbs->addItem("Producer", $this->get("router")->generate("producer_member_index"));
+    //     $breadcrumbs->addItem("Visits", $this->get("router")->generate("producer_visit_index"));
+    //     $breadcrumbs->addItem("Edit", $this->get("router")->generate("producer_visit_edit", array('id'=>$id)));
+
+    //     $visit = new Visit();
+
+    //     $form = $this->createForm(VisitType::class, $visit);
+
+    //     $form->handleRequest($request);
+
+    //     if ($form->isValid()) {
+    //         $em = $this->getDoctrine()->getManager();
+
+    //         $em->persist($visit);
+    //         $em->flush();
+
+    //         if (null === $response = $event->getResponse()) {
+    //             $url = $this->generateUrl('producer_visit_edit');
+    //             $response = new RedirectResponse($url);
+    //         }
+
+    //         return $response;
+    //     }
+
+    //     return $this->render('ProducerBundle:Visit:edit.html.twig', array(
+    //         'form' => $form->createView()
+    //     ));
+    // }
+
+    /**
+     * @Route("/{id}")
+     */
+    public function showAction(Request $request, Visit $visit)
     {
         $breadcrumbs = $this->get("white_october_breadcrumbs");
         $breadcrumbs->addItem("Home", $this->get("router")->generate("homepage"));
-        $breadcrumbs->addItem("Producer", $this->get("router")->generate("producer_member_index"));
+        $breadcrumbs->addItem("My Account", $this->get("router")->generate("producer_member_index"));
         $breadcrumbs->addItem("Visits", $this->get("router")->generate("producer_visit_index"));
-        $breadcrumbs->addItem("Edit", $this->get("router")->generate("producer_visit_edit", array('id'=>$id)));
+        $breadcrumbs->addItem("Show", $this->get("router")->generate("producer_visit_show", array('id'=>$visit->getId())));
 
-        $visit = new Visit();
-
-        $form = $this->createForm(VisitType::class, $visit);
-
-        $form->handleRequest($request);
-
-        if ($form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-
-            $em->persist($visit);
-            $em->flush();
-
-            if (null === $response = $event->getResponse()) {
-                $url = $this->generateUrl('producer_visit_edit');
-                $response = new RedirectResponse($url);
-            }
-
-            return $response;
-        }
-
-        return $this->render('ProducerBundle:Visit:edit.html.twig', array(
-            'form' => $form->createView()
+        return $this->render('ProducerBundle:Visit:show.html.twig', array(
+            'visit' => $visit,
+            'menu' => 'account'
         ));
     }
 }

39929178a4409860d33cb6b56f1a4e07ef16d2c6
Michael Hauptmann <mhauptma73@gmail.com>
2017-02-25 14:01:12 +0100

    Fixes

--- src/ManagementBundle/Form/MemberType.php
+++ src/ManagementBundle/Form/MemberType.php
@@ -6,7 +6,9 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
@@ -42,7 +44,7 @@ class MemberType extends AbstractType
             $form = $event->getForm();
 
             if( !$user || null === $user->getId() ){
-                $form->add('password', 'password');
+                $form->add('password', PasswordType::class);
             }
 
             $form
@@ -64,7 +66,7 @@ class MemberType extends AbstractType
             ;
 
             if( !$user || null === $user->getId() ){
-                $form->add('sendEmail', 'checkbox', array(
+                $form->add('sendEmail', CheckboxType::class, array(
                     'label' => 'Send Email',
                     'mapped' => false,
                     'required' => false

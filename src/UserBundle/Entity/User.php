446345b6a539f3bd0ecd6a7171189c07b1879f31
Michael Hauptmann <mhauptma73@gmail.com>
2017-03-04 15:39:06 +0100

    #51 Displaying latest visits

--- src/UserBundle/Entity/User.php
+++ src/UserBundle/Entity/User.php
@@ -116,9 +116,9 @@ class User implements UserInterface
     protected $passwordRequestedAt;
 
     /**
-     * @var Collection
+     /* @var Collection
      */
-    protected $groups;
+    // protected $groups;
 
     /**
      * @ORM\Column(type="boolean")
@@ -828,9 +828,9 @@ class User implements UserInterface
     {
         $roles = $this->roles;
 
-        foreach ($this->getGroups() as $group) {
-            $roles = array_merge($roles, $group->getRoles());
-        }
+        // foreach ($this->getGroups() as $group) {
+        //     $roles = array_merge($roles, $group->getRoles());
+        // }
 
         // we need to make sure to have at least one role
         $roles[] = static::ROLE_DEFAULT;
@@ -1051,55 +1051,55 @@ class User implements UserInterface
     /**
      * {@inheritdoc}
      */
-    public function getGroups()
-    {
-        return $this->groups ?: $this->groups = new ArrayCollection();
-    }
+    // public function getGroups()
+    // {
+    //     return $this->groups ?: $this->groups = new ArrayCollection();
+    // }
 
     /**
      * {@inheritdoc}
      */
-    public function getGroupNames()
-    {
-        $names = array();
-        foreach ($this->getGroups() as $group) {
-            $names[] = $group->getName();
-        }
+    // public function getGroupNames()
+    // {
+    //     $names = array();
+    //     foreach ($this->getGroups() as $group) {
+    //         $names[] = $group->getName();
+    //     }
 
-        return $names;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function hasGroup($name)
-    {
-        return in_array($name, $this->getGroupNames());
-    }
+    //     return $names;
+    // }
 
     /**
      * {@inheritdoc}
      */
-    public function addGroup(GroupInterface $group)
-    {
-        if (!$this->getGroups()->contains($group)) {
-            $this->getGroups()->add($group);
-        }
-
-        return $this;
-    }
+    // public function hasGroup($name)
+    // {
+    //     return in_array($name, $this->getGroupNames());
+    // }
 
     /**
      * {@inheritdoc}
      */
-    public function removeGroup(GroupInterface $group)
-    {
-        if ($this->getGroups()->contains($group)) {
-            $this->getGroups()->removeElement($group);
-        }
+    // public function addGroup(GroupInterface $group)
+    // {
+    //     if (!$this->getGroups()->contains($group)) {
+    //         $this->getGroups()->add($group);
+    //     }
 
-        return $this;
-    }
+    //     return $this;
+    // }
+
+    /**
+     * {@inheritdoc}
+     */
+    // public function removeGroup(GroupInterface $group)
+    // {
+    //     if ($this->getGroups()->contains($group)) {
+    //         $this->getGroups()->removeElement($group);
+    //     }
+
+    //     return $this;
+    // }
 
     /**
      * {@inheritdoc}
